a85acc395ffecd3299d922d267fede448ebfd4f4
Rasheed Ahmad <118961857+Rasheed-Ahmad07@users.noreply.github.com>
2024-01-05 23:33:13 +0500

    Add files via upload

---
+++ 003 arthematic operaters/index.js
@@ -0,0 +1,46 @@
+// ARTHEMATIC OPERATERS
+// +,-,* ,/,**,%, ++ ,--
+
+
+// let x=2;
+// let y=4;
+// let z=x+y;
+// let w=x-y;
+// let v=x*y;
+// let d=x/y;
+// let e=x%y;
+// let a=x**y;
+// console.log(z);
+// console.log(w);
+// console.log(v);
+// console.log(d);
+// console.log(e);
+// console.log(a);
+
+
+
+// let x=3;
+// let y=8;
+// let z=x/y;
+// console.log(z);
+
+
+// let x= 4 +2 +5;
+// console.log(x);
+
+
+
+// let x= "2"+2 +5;
+// console.log(x);
+
+// let z= 12**3;
+// console.log(z);
+
+// let z= 36 % 5;
+// console.log(z);
+
+// let x=30
+// let y=89;
+// let z= y%x;
+// console.log(z);
+
